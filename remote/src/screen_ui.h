cb2c3da815aea212c0f15ab83be6cba64318069d
Lena Voytek <lena@voytek.dev>
2026-08-05 18:03:52 -0400

    Add up and down arrow sprites

--- remote/src/screen_ui.h
+++ remote/src/screen_ui.h
@@ -3,6 +3,13 @@
 
 #include <stdbool.h>
 
+// The set of available sprites to display.
+typedef enum {
+	TIMECHIME_SPRITE_ARROW_UP,
+	TIMECHIME_SPRITE_ARROW_DOWN,
+	NUM_TIMECHIME_SPRITES
+} timechime_sprite_t;
+
 // Initialize screen hardware and LVGL and confirm device is available.
 bool timechime_screen_init();
 
---
+++ remote/src/sprites.h
@@ -0,0 +1,9 @@
+#ifndef TIMECHIME_SPRITES_H
+#define TIMECHIME_SPRITES_H
+
+#include <lvgl.h>
+
+extern const lv_image_dsc_t sprite_up;
+extern const lv_image_dsc_t sprite_down;
+
+#endif
