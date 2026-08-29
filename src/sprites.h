b374ab8f50436ed063d985d6279e44567ec3ca8d
Lena Voytek <lena@voytek.dev>
2026-08-29 17:26:53 -0400

    Add time sprite

--- src/sprites.h
+++ src/sprites.h
@@ -11,6 +11,7 @@ extern const lv_image_dsc_t sprite_gear;
 extern const lv_image_dsc_t sprite_sound_select;
 extern const lv_image_dsc_t sprite_toggle_on;
 extern const lv_image_dsc_t sprite_toggle_off;
+extern const lv_image_dsc_t sprite_time;
 extern const lv_image_dsc_t sprite_trash;
 extern const lv_image_dsc_t sprite_confirm;
 extern const lv_image_dsc_t sprite_gpssearch;
